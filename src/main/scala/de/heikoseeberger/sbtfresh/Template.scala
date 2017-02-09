4e72c30c9753ac173af4ee205b3c95f275c7cf92
Heiko Seeberger <mail@heikoseeberger.de>
2017-02-09 18:23:45 +0100

    Upgrade scalafmt to 0.5.6

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -264,7 +264,7 @@ private object Template {
 
   def plugins: String =
     """|addSbtPlugin("com.dwijnand"      % "sbt-travisci" % "1.0.0")
-       |addSbtPlugin("com.geirsson"      % "sbt-scalafmt" % "0.5.5")
+       |addSbtPlugin("com.geirsson"      % "sbt-scalafmt" % "0.5.6")
        |addSbtPlugin("com.typesafe.sbt"  % "sbt-git"      % "0.8.5")
        |addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "1.6.0")
        |""".stripMargin
