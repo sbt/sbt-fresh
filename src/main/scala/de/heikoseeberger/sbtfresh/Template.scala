387aa0a998321ca4788e3f56462708a1aff0ab15
Heiko Seeberger <mail@heikoseeberger.de>
2017-04-07 18:09:52 +0200

    Upgrade to sbt-git 0.9.2

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -281,13 +281,15 @@ private object Template {
   def plugins(setUpTravis: Boolean): String = {
     val travisPlugin =
       if (setUpTravis)
-        """|addSbtPlugin("com.dwijnand"      % "sbt-travisci" % "1.0.0")
+        """|addSbtPlugin("com.dwijnand"      % "sbt-travisci" % "1.1.0")
            |""".stripMargin
       else ""
 
     s"""|${travisPlugin}addSbtPlugin("com.geirsson"      % "sbt-scalafmt" % "0.6.6")
-        |addSbtPlugin("com.typesafe.sbt"  % "sbt-git"      % "0.9.0")
+        |addSbtPlugin("com.typesafe.sbt"  % "sbt-git"      % "0.9.2")
         |addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "1.8.0")
+        |
+        |libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
         |""".stripMargin
   }
 
