1eac0e8cf8ffc8e861c28d8b94153c279e04e6e2
Heiko Seeberger <mail@heikoseeberger.de>
2017-08-11 16:28:49 +0200

    Upgrade to sbt 1.0.0

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -24,7 +24,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 0.13.16
+    """|sbt.version = 1.0.0
        |""".stripMargin
 
   def buildSbt(organization: String,
@@ -213,7 +213,7 @@ private object Template {
 
     s"""|$travisPlugin${wartRemoverPlugin}addSbtPlugin("com.typesafe.sbt"  % "sbt-git"         % "0.9.3")
         |addSbtPlugin("com.lucidchart"    % "sbt-scalafmt"    % "1.10")
-        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "2.0.0")
+        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "3.0.0")
         |
         |libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
         |""".stripMargin
