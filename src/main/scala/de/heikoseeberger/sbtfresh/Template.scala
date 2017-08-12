c6f270f8d59a76f6c5e2d5539d1252736240141b
Heiko Seeberger <hseeberger@users.noreply.github.com>
2017-08-12 22:56:30 +0200

    Upgrade to sbt-header 3.0.1 (#106)

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -213,7 +213,7 @@ private object Template {
 
     s"""|$travisPlugin${wartRemoverPlugin}addSbtPlugin("com.typesafe.sbt"  % "sbt-git"         % "0.9.3")
         |addSbtPlugin("com.lucidchart"    % "sbt-scalafmt"    % "1.10")
-        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "3.0.0")
+        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "3.0.1")
         |
         |libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
         |""".stripMargin
