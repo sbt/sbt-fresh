093f83cadadaae9c762e43ab021318f9f431d251
Heiko Seeberger <mail@heikoseeberger.de>
2017-09-20 16:11:45 +0200

    Sort plugins alphabetically

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -198,13 +198,13 @@ private object Template {
       else ""
     val wartRemoverPlugin =
       if (setUpWartremover)
-        """|addSbtPlugin("org.wartremover"   % "sbt-wartremover" % "2.2.0")
-           |""".stripMargin
+        """|
+           |addSbtPlugin("org.wartremover"   % "sbt-wartremover" % "2.2.0")""".stripMargin
       else ""
 
-    s"""|$travisPlugin${wartRemoverPlugin}addSbtPlugin("com.typesafe.sbt"  % "sbt-git"         % "0.9.3")
-        |addSbtPlugin("com.lucidchart"    % "sbt-scalafmt"    % "1.12")
-        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "3.0.2")
+    s"""|${travisPlugin}addSbtPlugin("com.lucidchart"    % "sbt-scalafmt"    % "1.12")
+        |addSbtPlugin("com.typesafe.sbt"  % "sbt-git"         % "0.9.3")
+        |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "3.0.2")${wartRemoverPlugin}
         |
         |libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
         |""".stripMargin
