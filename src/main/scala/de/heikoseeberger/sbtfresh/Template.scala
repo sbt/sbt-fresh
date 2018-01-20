3ea8c4b108e41e6bd4cef8d281d244f1f29cac1f
Heiko Seeberger <mail@heikoseeberger.rocks>
2018-01-20 12:00:08 +0100

    Add compiler option -Ywarn-unused-import

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -113,7 +113,8 @@ private object Template {
         |      "-language:_",
         |      "-target:jvm-1.8",
         |      "-encoding", "UTF-8",
-        |      "-Ypartial-unification"
+        |      "-Ypartial-unification",
+        |      "-Ywarn-unused-import"
         |    ),
         |    Compile / unmanagedSourceDirectories := Seq((Compile / scalaSource).value),
         |    Test / unmanagedSourceDirectories := Seq((Test / scalaSource).value),
