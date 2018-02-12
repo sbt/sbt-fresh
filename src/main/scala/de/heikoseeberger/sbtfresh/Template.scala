c67e5e33d5b436b87fc916050fda76998985a06f
Heiko Seeberger <hseeberger@users.noreply.github.com>
2018-02-12 11:10:54 +0100

    Upgrade to sbt 1.1.1 (#131)

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -24,7 +24,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 1.1.0
+    """|sbt.version = 1.1.1
        |""".stripMargin
 
   def buildSbt(organization: String,
