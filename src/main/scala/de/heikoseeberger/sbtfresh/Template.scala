d9b43fc2c880064cb52db0c5d819d83f461afb1c
Heiko Seeberger <mail@heikoseeberger.rocks>
2018-03-22 10:43:45 -0400

    Upgrade to utest 0.6.4

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -83,7 +83,7 @@ private object Template {
         |  new {
         |    object Version {
         |      val scalaCheck = "1.13.5"
-        |      val utest      = "0.6.3"
+        |      val utest      = "0.6.4"
         |    }
         |    val scalaCheck = "org.scalacheck" %% "scalacheck" % Version.scalaCheck
         |    val utest      = "com.lihaoyi"    %% "utest"      % Version.utest
