fa68ff566a720ff6f646fd0af68b46c6c15e883c
Heiko Seeberger <mail@heikoseeberger.rocks>
2018-05-10 22:28:19 +0200

    Upgrade to sbt 1.1.5

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -24,7 +24,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 1.1.4
+    """|sbt.version = 1.1.5
        |""".stripMargin
 
   def buildSbt(organization: String,
