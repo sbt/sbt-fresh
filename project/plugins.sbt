063df5505b34c2cd928f2e2b4f11480a9c56915d
Heiko Seeberger <hseeberger@users.noreply.github.com>
2019-02-21 16:53:54 +0100

    Bump versions of some plugins (#158)

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
-addSbtPlugin("com.dwijnand"      % "sbt-dynver"   % "3.1.0")
+addSbtPlugin("com.dwijnand"      % "sbt-dynver"   % "3.3.0")
 addSbtPlugin("com.dwijnand"      % "sbt-travisci" % "1.1.3")
 addSbtPlugin("com.geirsson"      % "sbt-scalafmt" % "1.5.1")
 addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "5.1.0")
 addSbtPlugin("org.foundweekends" % "sbt-bintray"  % "0.5.4")
 
-libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
+libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.26" // Needed by sbt-git
--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -198,11 +198,9 @@ private object Template {
       else
         ""
 
-    s"""|addSbtPlugin("com.dwijnand"      % "sbt-dynver"      % "3.1.0")${travisPlugin}
+    s"""|addSbtPlugin("com.dwijnand"      % "sbt-dynver"      % "3.3.0")${travisPlugin}
         |addSbtPlugin("com.geirsson"      % "sbt-scalafmt"    % "1.5.1")
         |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "5.1.0")${wartRemoverPlugin}
-        |
-        |libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.25" // Needed by sbt-git
         |""".stripMargin
   }
 
