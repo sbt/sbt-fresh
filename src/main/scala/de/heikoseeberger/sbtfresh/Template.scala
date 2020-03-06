7100bdcd2c9e375f347a1b0f214606e5248335cb
Heiko Seeberger <mail@heikoseeberger.rocks>
2020-03-06 17:05:24 +0100

    Bump sbt-scalafmt to 2.3.2

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -190,7 +190,7 @@ private object Template {
 
     s"""|addSbtPlugin("com.dwijnand"      % "sbt-dynver"      % "4.0.0")${travisPlugin}
         |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "5.4.0")
-        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt"    % "2.3.1")${wartRemoverPlugin}
+        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt"    % "2.3.2")${wartRemoverPlugin}
         |""".stripMargin
   }
 
