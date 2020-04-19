4fef290b4587756ff36c2613b0e6090c8c8913df
Scala Steward <me@scala-steward.org>
2020-04-19 16:32:37 +0200

    Update sbt-scalafmt to 2.3.4

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -189,7 +189,7 @@ private object Template {
 
     s"""|addSbtPlugin("com.dwijnand"      % "sbt-dynver"      % "4.0.0")${travisPlugin}
         |addSbtPlugin("de.heikoseeberger" % "sbt-header"      % "5.4.0")
-        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt"    % "2.3.2")${wartRemoverPlugin}
+        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt"    % "2.3.4")${wartRemoverPlugin}
         |""".stripMargin
   }
 
