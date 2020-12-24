be66918b35d2fa9a64748dc2a3516987b64e4cb0
Heiko Seeberger <mail@heikoseeberger.rocks>
2020-12-24 15:35:40 +0100

    Bump sbt to 1.4.6

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -24,7 +24,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 1.4.5
+    """|sbt.version = 1.4.6
        |""".stripMargin
 
   def buildSbt(
