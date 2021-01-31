cd29d3ab338edec642450378c81aa3959270d1d5
Heiko Seeberger <hseeberger@users.noreply.github.com>
2021-01-31 18:50:32 +0100

    Bump sbt to 1.4.7 in Template (#262)

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -24,7 +24,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 1.4.6
+    """|sbt.version = 1.4.7
        |""".stripMargin
 
   def buildSbt(
