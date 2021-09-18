29edaf6ca6f5f59dc15009549712bba0ba3151d7
Heiko Seeberger <git@heikoseeberger.rocks>
2021-09-18 11:48:55 +0200

    feat: bump scalafmt to 3.0.4

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -199,7 +199,7 @@ private object Template {
   }
 
   def scalafmtConf: String =
-    """|version = "3.0.3"
+    """|version = "3.0.4"
        |
        |preset         = "defaultWithAlign"
        |runner.dialect = "scala3"
