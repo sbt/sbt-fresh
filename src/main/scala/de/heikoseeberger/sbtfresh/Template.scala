a28094cbfd1a904db9bf14b8169cbe2da6ddd1e4
Heiko Seeberger <git@heikoseeberger.rocks>
2021-12-03 13:02:53 +0100

    feat: update scalafmt to 3.2.1

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -199,7 +199,7 @@ private object Template {
   }
 
   def scalafmtConf: String =
-    """|version = "3.2.0"
+    """|version = "3.2.1"
        |
        |preset         = "defaultWithAlign"
        |runner.dialect = "scala3"
