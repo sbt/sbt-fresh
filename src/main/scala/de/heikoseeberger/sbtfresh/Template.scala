c38a366e0d3a232d76ee8e210f57e6cfff1b0d47
Scala Steward <me@scala-steward.org>
2021-12-04 13:37:29 +0100

    Update sbt-scalafmt to 2.4.5

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -167,7 +167,7 @@ private object Template {
   def plugins: String =
     s"""|addSbtPlugin("com.dwijnand"      % "sbt-dynver"   % "4.1.1")
         |addSbtPlugin("de.heikoseeberger" % "sbt-header"   % "5.6.0")
-        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.4.4")
+        |addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.4.5")
         |""".stripMargin
 
   def readme(name: String, license: Option[License]): String = {
