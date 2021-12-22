ec65dfc7a0e68fc210e0a2d2a924a4b4fea9a23c
Heiko Seeberger <git@heikoseeberger.rocks>
2021-12-22 10:39:16 +0100

    feat: update sbt to 1.5.8

--- src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
+++ src/main/scala/de/heikoseeberger/sbtfresh/Template.scala
@@ -23,7 +23,7 @@ private object Template {
   private val year = now().getYear
 
   def buildProperties: String =
-    """|sbt.version = 1.5.6
+    """|sbt.version = 1.5.8
        |""".stripMargin
 
   def buildSbt(
